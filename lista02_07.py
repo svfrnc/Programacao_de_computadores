b6408ab33975b7b247156b22ab12195a8e60dce7
Sávio França <164408156+svfrnc@users.noreply.github.com>
2025-10-06 18:00:48 -0300

    Add files via upload

---
+++ lista02_07.py
@@ -0,0 +1,3 @@
+T1 , T2 , T3 , T4 = map(int,input().split())
+Total_Tomadas= T1+(T2-1)+(T3-1)+(T4-1)
+print(Total_Tomadas)
---
+++ lista02_08.py
@@ -0,0 +1,3 @@
+metros_corridos , comprimento_pista = map(int,input().split())
+resto_corrida= metros_corridos % comprimento_pista
+print(resto_corrida)
